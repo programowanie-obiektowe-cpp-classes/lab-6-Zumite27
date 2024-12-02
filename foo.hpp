75f71960822fdfaab1e430d613fe5c1855acf6bd
Zuzanna Żakowska <zakowska.zuzanna@gmail.com>
2024-12-02 18:47:57 +0100

    Funkcja foo.hpp

--- foo.hpp
+++ foo.hpp
@@ -11,6 +11,8 @@ std::vector< char > foo(std::list< Human >& people)
     std::vector< char > retval(people.size());
 
     // Twoja implementacja tutaj
+    std::for_each(people.begin(), people.end(), [](Human& person) { person.birthday(); });
+    std::transform(people.rbegin(), people.rend(), retval.begin(), [](const Human& person){return person.isMonster() ? 'n' : 'y';});
 
     return retval;
 }
